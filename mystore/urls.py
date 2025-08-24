e7e1349b38662369318d09647d5a82833ce37c82
Sergei <graybas1993@gmail.com>
2025-08-24 14:28:47 +0300

    Registered simple tag for categories

--- mystore/urls.py
+++ mystore/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf.urls.static import static
+
+from mystore import settings
 
 
 urlpatterns = [ 
@@ -24,4 +27,9 @@ urlpatterns = [
     path('', include('main.urls', namespace='main')),
     path('catalog/', include('goods.urls', namespace='catalog'))
 
-] + debug_toolbar_urls()
+]
+
+
+if settings.DEBUG:
+    urlpatterns += debug_toolbar_urls()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
